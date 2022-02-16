e1e90ce2abcc82392dd73e6e5c4afd61909dc65c
Stévillis Sousa <stevillis@hotmail.com>
2022-02-16 11:01:44 -0400

    Create Component TextCounter

---
+++ src/TextCounter.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TextCounter extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Meu contador</h1>
+                <textarea />
+                <div>
+                    <strong>Total:</strong>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TextCounter;

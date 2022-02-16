80db6a92c6de0d4f89b1ae4f8968a6d1968ad0d9
Stévillis Sousa <stevillis@hotmail.com>
2022-02-16 12:01:17 -0400

    Create props

--- src/TextCounter.js
+++ src/TextCounter.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 
 class TextCounter extends Component {
+    static defaultProps = {
+        limit: 15
+    }
+
     constructor(props) {
         super(props);
+
         this.state = {
             totalChars: 0
         }
     }
 
     render() {
-        const { state } = this;
+        const { props, state } = this;
         return (
             <div>
                 <h1>Meu contador</h1>
                 <textarea />
                 <div>
-                    <strong>Total:</strong> {state.totalChars}
+                    <strong>Total:</strong> {state.totalChars} / {props.limit}
                 </div>
             </div>
         );
